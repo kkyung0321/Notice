1f47e2f869e688a5e6951d6fb1a915ad5617cc42
kiyoung kim <kkyung0321@gmail.com>
2022-02-01 03:48:59 +0900

    feat: Refactor test code

--- src/main/java/com/example/studywithme/reply/application/interact/ReplyService.java
+++ src/main/java/com/example/studywithme/reply/application/interact/ReplyService.java
@@ -1,14 +1,13 @@
 package com.example.studywithme.reply.application.interact;
 
 import com.example.studywithme.global.auth.UserDto;
-import com.example.studywithme.member.application.entity.Member;
 import com.example.studywithme.reply.application.dto.ReplyRequest;
 import com.example.studywithme.reply.application.dto.ReplyResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ReplyService {
-    void writeReply(UserDto userDto, Long pid, ReplyRequest replyRequest);
+    void writeReply(com.example.studywithme.global.auth.UserDto userDto, Long pid, ReplyRequest replyRequest);
 
     void modifyReply(Long rid, ReplyRequest replyRequest);
 
@@ -16,5 +15,5 @@ public interface ReplyService {
 
     Page<ReplyResponse> readReplies(Long pid, Pageable pageable);
 
-    Page<ReplyResponse> readMyReplies(Member member, Pageable pageable);
+    Page<ReplyResponse> readMyReplies(UserDto userDto, Pageable pageable);
 }
